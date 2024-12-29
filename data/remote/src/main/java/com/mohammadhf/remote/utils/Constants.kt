1f154e28c1c6d64b2ae6d46b9bcdc50892920f75
MohammadFakhraee <mohammadfakhraee007@gmail.com>
2024-12-29 23:37:19 +0330

    Implement Cats data source with tests

---
+++ data/remote/src/main/java/com/mohammadhf/remote/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.mohammadhf.remote.utils
+
+const val CATS_BASE_URL = "https://api.thecatapi.com/"
+
+const val APP_API_KEY = "live_tjch2mIz739lpBhRltSHCFXubHhct011s8K4Syb8S357bnD8IBkFZxEnzW62kgYt"
